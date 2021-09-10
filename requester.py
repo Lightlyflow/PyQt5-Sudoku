4b713909d04bcf5882d401d11334992809e562db
Lightlyflow <tongqu777@gmail.com>
2021-09-10 12:30:17 -0500

    Added urls for each type of difficulty.

--- requester.py
+++ requester.py
@@ -16,5 +16,21 @@ def getUrl(diff: Difficulty) -> str:
     return f"{ROOT}{diff.value}"
 
 
+def getEasyUrl() -> str:
+    return f"{ROOT}{Difficulty.EASY}"
+
+
+def getMediumUrl() -> str:
+    return f"{ROOT}{Difficulty.MEDIUM}"
+
+
+def getHardUrl() -> str:
+    return f"{ROOT}{Difficulty.HARD}"
+
+
+def getRandomUrl() -> str:
+    return f"{ROOT}{Difficulty.RANDOM}"
+
+
 if __name__ == '__main__':
     print()
